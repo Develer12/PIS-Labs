cc8cc4d9a24681c74cce0415a821baa525880b17
Develer12 <cenia-v@mail.ru>
2020-02-27 10:58:15 +0300

    Completed

---
+++ Lab2a/Lab2s/App_Code/PostHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class PostHandler : IHttpHandler
+{
+    public void ProcessRequest(HttpContext context)
+    {
+        HttpRequest req = context.Request;
+        HttpResponse res = context.Response;
+        res.AppendHeader("Access-Control-Allow-Origin", "*");
+        res.AppendHeader("Access-Control-Allow-Credentials", "true");
+        int a = Convert.ToInt32(req.Params.Get("a"));
+        int b = Convert.ToInt32(req.Params.Get("b"));
+        res.Write("PostHandler: " + (a + b));
+    }
+    public bool IsReusable
+    {
+        get { return false; }
+    }
+}
