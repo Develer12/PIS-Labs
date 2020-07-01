314eba66eb896a67d2867ebe06e5351ae22fec44
Develer12 <cenia-v@mail.ru>
2020-07-01 12:33:58 +0300

    Complete 17

---
+++ EKZ/17.DI/TelephoneDll/TelephoneDictionary.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace TelephoneDll
+{
+    public class TelephoneDictionary : IPhoneDictionary
+    {
+        private List<Telephone> telephones = new List<Telephone>();
+
+        private List<Telephone> Telephones { get => telephones; set => telephones = value; }
+
+        public TelephoneDictionary()
+        {
+
+        }
+
+        public List<Telephone> selectAll()
+        {
+            string jsonString = File.ReadAllText("D:\\Subject\\ПИС\\Lab\\Lab6json\\Lab3new\\App_Data\\numbers.json");
+            this.telephones = JsonConvert.DeserializeObject<List<Telephone>>(jsonString).ToList();
+            return telephones.OrderBy(u => u.surname).ToList();
+        }
+    }
+}
